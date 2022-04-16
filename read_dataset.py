e5de776d2c6482c7820e4ed47e381185e9a8abd4
l3dev <ylebedev@gmail.com>
2022-04-15 20:14:56 -0400

    flow network

---
+++ read_dataset.py
@@ -0,0 +1,69 @@
+import numpy as np
+from scipy.io import loadmat
+
+
+# ******************************************************************************
+# Read in data
+# ******************************************************************************
+def data_from_name(name):
+    if name == 'flow_cylinder':
+        return flow_cylinder()
+
+    else:
+        raise ValueError('dataset {} not recognized'.format(name))
+
+
+def rescale(Xsmall, Xsmall_test):
+    # ******************************************************************************
+    # Rescale data
+    # ******************************************************************************
+    Xmin = Xsmall.min()
+    Xmax = Xsmall.max()
+
+    Xsmall = ((Xsmall - Xmin) / (Xmax - Xmin))
+    Xsmall_test = ((Xsmall_test - Xmin) / (Xmax - Xmin))
+
+    return Xsmall, Xsmall_test
+
+
+def flow_cylinder():
+    #X = np.load('data/flow_cylinder.npy')
+    X = np.load('data/cyl2dflow.npy')
+    print(X.shape)
+
+    # Split into train and test set
+    # Xsmall = X[0:100, :, :]
+    # t, m, n = Xsmall.shape
+    #Xsmall = X[0:700, 0, 100:300, 100:700 ]
+    Xsmall = X[0:700, 0, 100:300, 130:500]
+    t, n, m = Xsmall.shape
+
+    #Xsmall = Xsmall.reshape(100, -1)
+    #Xsmall = Xsmall.reshape(700, -1)
+    Xsmall = Xsmall.reshape(700, -1)
+
+    #Xsmall_test = X[100:151, :, :].reshape(51, -1)
+    Xsmall_test = X[700:1000,0, 100:300, 130:500 ].reshape(300, -1)
+
+    # ******************************************************************************
+    # Return train and test set
+    # ******************************************************************************
+    return Xsmall, Xsmall_test, m, n
+
+# def flow_cylinder():
+#     X = np.load('data/flow_cylinder.npy')
+#     #X = np.load('data/cyl2dflow_old.npy')
+#     print(X.shape)
+#
+#     # Split into train and test set
+#     Xsmall = X[0:100, :, :]
+#     t, m, n = Xsmall.shape
+#
+#     Xsmall = Xsmall.reshape(100, -1)
+#
+#     Xsmall_test = X[100:151, :, :].reshape(51, -1)
+#
+#     # ******************************************************************************
+#     # Return train and test set
+#     # ******************************************************************************
+#     return Xsmall, Xsmall_test, m, n
---
+++ shallowdecoder_model.py
@@ -0,0 +1,98 @@
+from torch import nn
+
+
+def model_from_name(name, outputlayer_size, n_sensors):
+    if name == 'shallow_decoder':
+        return ShallowDecoder(outputlayer_size, n_sensors)
+
+    elif name == 'shallow_decoder_drop':
+        return ShallowDecoderDrop(outputlayer_size, n_sensors)
+
+    raise ValueError('model {} not recognized'.format(name))
+
+
+class ShallowDecoder(nn.Module):
+    def __init__(self, outputlayer_size, n_sensors):
+        super(ShallowDecoder, self).__init__()
+
+        self.n_sensors = n_sensors
+        self.outputlayer_size = outputlayer_size
+
+        self.learn_features = nn.Sequential(
+            nn.Linear(n_sensors, 100),
+            nn.ReLU(True),
+            nn.BatchNorm1d(1),
+        )
+
+        self.learn_coef = nn.Sequential(
+            nn.Linear(100, 150),
+            nn.ReLU(True),
+            nn.BatchNorm1d(1),
+        )
+
+        self.learn_dictionary = nn.Sequential(
+            nn.Linear(150, self.outputlayer_size),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
+                if m.bias is not None:
+                    nn.init.constant(m.bias, 0.0)
+
+    def forward(self, x):
+        x = self.learn_features(x)
+        x = self.learn_coef(x)
+        x = self.learn_dictionary(x)
+        return x
+
+
+class ShallowDecoderDrop(nn.Module):
+    def __init__(self, outputlayer_size, n_sensors):
+        super(ShallowDecoderDrop, self).__init__()
+
+        self.n_sensors = n_sensors
+        self.outputlayer_size = outputlayer_size
+
+        self.learn_features = nn.Sequential(
+            nn.Linear(n_sensors, 2000),
+            nn.ReLU(True),
+            nn.BatchNorm1d(1),
+        )
+
+        self.learn_coef = nn.Sequential(
+            nn.Linear(2000, 2500),
+            nn.ReLU(True),
+            nn.BatchNorm1d(1),
+        )
+
+        self.learn_dictionary = nn.Sequential(
+            nn.Linear(2500, self.outputlayer_size),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight)
+                if m.bias is not None:
+                    nn.init.constant(m.bias, 0.0)
+
+    def forward(self, x):
+        x = self.learn_features(x)
+        x = nn.functional.dropout(x, p=0.1, training=self.training)
+        x = self.learn_coef(x)
+        x = self.learn_dictionary(x)
+        return x
